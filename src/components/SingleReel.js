16823040ce32c6c605c132efd341a88fe08e2176
kanchanMalviya <kanchan@newrise.in>
2023-02-08 13:56:53 +0530

    creating favorite page

--- src/components/SingleReel.js
+++ src/components/SingleReel.js
@@ -9,9 +9,13 @@ import {
 import React, {useState, useRef} from 'react';
 import Video from 'react-native-video';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Feather from 'react-native-vector-icons/Feather';
 
 const SingleReel = ({item, index, currentIndex}) => {
   const [mute, setMute] = useState(false);
+  const [like, setLike] = useState(item.isliked);
+
   const windowWidth = Dimensions.get('window').width;
   const windowHeight = Dimensions.get('window').height;
 
@@ -40,7 +44,7 @@ const SingleReel = ({item, index, currentIndex}) => {
           onError={onError}
           repeat={true}
           resizeMode="cover"
-          paused={false}
+          paused={currentIndex == index ? false : true}
           muted={mute}
           source={item.video}
           style={{width: '100%', height: '100%', position: 'absolute'}}
@@ -55,7 +59,7 @@ const SingleReel = ({item, index, currentIndex}) => {
             position: 'absolute',
             top: windowHeight / 2.3,
             left: windowWidth / 2.3,
-            backgroundColor: 'rgba(52,52,52,0.6',
+            backgroundColor: 'rgba(52,52,52,0.6)',
             borderRadius: 100,
             padding: mute ? 10 : 0,
           }}
@@ -107,6 +111,59 @@ const SingleReel = ({item, index, currentIndex}) => {
             </TouchableOpacity>
             <Text style={{color: 'white'}}>Original Video</Text>
           </View>
+        </View>
+      </View>
+      <View
+        style={{
+          position: 'absolute',
+          bottom: 60,
+          right: 0,
+          alignItems: 'center',
+          flexDirection: 'column',
+        }}>
+        <TouchableOpacity onPress={() => setLike(!like)} style={{padding: 10}}>
+          <AntDesign
+            name={like ? 'heart' : 'hearto'}
+            style={{color: like ? 'red' : 'white', fontSize: 25}}
+          />
+          <Text style={{color: 'white', marginTop: 5}}>{item?.likes}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={{padding: 10}}>
+          <Ionicons
+            name="ios-chatbubble-outline"
+            style={{color: 'white', fontSize: 25}}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={{padding: 10}}>
+          <Ionicons
+            name="paper-plane-outline"
+            style={{color: 'white', fontSize: 25}}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={{padding: 10}}>
+          <Feather
+            name="more-vertical"
+            style={{color: 'white', fontSize: 25}}
+          />
+        </TouchableOpacity>
+        <View
+          style={{
+            borderRadius: 10,
+            width: 30,
+            height: 30,
+            borderWidth: 2,
+            margin: 10,
+            borderColor: 'white',
+          }}>
+          <Image
+            source={item.nameimage}
+            style={{
+              borderRadius: 10,
+              width: '100%',
+              height: '100%',
+              resizeMode: 'cover',
+            }}
+          />
         </View>
       </View>
     </View>
--- src/screens/FavoriteScreen.js
+++ src/screens/FavoriteScreen.js
@@ -1,10 +1,74 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {friendsProfileData} from '../../Videos';
 
 const FavoriteScreen = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setData(friendsProfileData);
+  }, []);
+
   return (
-    <View>
-      <Text>FavoriteScreen</Text>
+    <View style={{width: '100%', height: '100%', backgroundColor: 'white'}}>
+      <Text
+        style={{
+          fontSize: 20,
+          fontWeight: 'bold',
+          borderBottomWidth: 0.5,
+          borderBottomColor: '#DEDEDE',
+          padding: 10,
+          margin: 10,
+        }}>
+        Activity
+      </Text>
+      <ScrollView style={{margin: 10}}>
+        <Text style={{fontWeight: 'bold'}}> This Week</Text>
+        <View style={{flexDirection: 'row', paddingVertical: 10}}>
+          {data.slice(0, 3).map((item, index) => {
+            return (
+              <TouchableOpacity key={index}>
+                <Text>{item.name}</Text>
+              </TouchableOpacity>
+            );
+          })}
+          <Text>Started following you</Text>
+        </View>
+        <Text style={{fontWeight: 'bold'}}>Earlier</Text>
+
+        {data.slice(0, 3).map((item, index) => {
+          return (
+            <View key={index}>
+              <View>
+                <TouchableOpacity
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    maxWidth: '64%',
+                  }}>
+                  <Image
+                    source={item.profileImage}
+                    style={{
+                      width: 45,
+                      height: 45,
+                      borderRadius: 100,
+                      marginRight: 10,
+                    }}
+                  />
+                </TouchableOpacity>
+              </View>
+            </View>
+          );
+        })}
+      </ScrollView>
     </View>
   );
 };
